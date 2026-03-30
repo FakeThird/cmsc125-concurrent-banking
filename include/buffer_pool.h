fa26f41fda532e387774423027e77347f2c2c59c
FakeThird <thirdwheelerforhire@gmail.com>
2026-03-30 23:56:26 +0800

    feat: Initialized buffer pool header structures and dependencies

---
+++ include/buffer_pool.h
@@ -0,0 +1,23 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <pthread.h>
+#include <semaphore.h>
+#include "bank.h"
+
+#define BUFFER_POOL_SIZE 5
+
+typedef struct {
+    int account_id;
+    Account* data;
+    bool in_use;
+} BufferSlot;
+
+typedef struct {
+    BufferSlot slots[BUFFER_POOL_SIZE];
+    sem_t empty_slots;
+    sem_t full_slots;
+    pthread_mutex_t pool_lock;
+} BufferPool;
+
+#endif
