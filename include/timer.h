12e648877e884693aea2eb29ba7258a822ded70a
“CL4-Bisk” <apariciojohnclyde@gmail.com>
2026-04-22 17:33:37 +0800

    chore: added an all transactions done checker for timer

--- include/timer.h
+++ include/timer.h
@@ -4,6 +4,7 @@
 #define MAX_ACCOUNTS 100
 
 extern volatile int global_tick;
+extern volatile int all_transactions_done;
 
 void* timer_thread(void* arg);
 void wait_until_tick(int target_tick);
--- src/timer.c
+++ src/timer.c
@@ -4,28 +4,36 @@
 #include <pthread.h>
 
 volatile int global_tick = 0;
+volatile int all_transactions_done = 0;
 pthread_mutex_t tick_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t tick_changed = PTHREAD_COND_INITIALIZER;
 
 // Timer thread increments clock every TICK_INTERVAL_MS
-void* timer_thread(void* arg) {
+void *timer_thread(void *arg)
+{
     // Actual Code: while (simulation_running)
-    int tick_interval_ms = *(int*)arg; // Get tick interval from argument
-    while (1) {
+    int tick_interval_ms = *(int *)arg; // Get tick interval from argument
+    while (!all_transactions_done)
+    {
+        usleep(tick_interval_ms * 1000); // Sleep to simulate a tick
+
         pthread_mutex_lock(&tick_lock);
-        // Actual Code: usleep(TICK_INTERVAL_MS * 1000); 
-        usleep(tick_interval_ms * 1000);  // Sleep to simulate a tick
         global_tick++;
-        pthread_cond_broadcast(&tick_changed);  // Wake waiting
+
+        // Wake all transactions waiting for this tick
+        pthread_cond_broadcast(&tick_changed); // Wake waiting
+
         pthread_mutex_unlock(&tick_lock);
     }
     return NULL;
 }
 
 // Transactions wait until their start_tick
-void wait_until_tick(int target_tick) {
+void wait_until_tick(int target_tick)
+{
     pthread_mutex_lock(&tick_lock);
-    while (global_tick < target_tick) {
+    while (global_tick < target_tick)
+    {
         pthread_cond_wait(&tick_changed, &tick_lock);
     }
     pthread_mutex_unlock(&tick_lock);
