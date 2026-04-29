3484afffeac9b528d6194601176486a88eb6a839
“CL4-Bisk” <apariciojohnclyde@gmail.com>
2026-04-29 14:04:36 +0800

    meta: initialized print_metrics() header function

--- include/metrics.h
+++ include/metrics.h
@@ -2,6 +2,9 @@
 #ifndef METRICS_H
 #define METRICS_H
 
+#include "../include/transaction.h"
+
 void print_accounts_to_file(const char *filename);
+void print_metrics(Transaction *transactions, int num_transactions);
 
 #endif
